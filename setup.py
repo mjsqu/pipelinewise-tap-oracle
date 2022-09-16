8a0fa82e70a231f633b78e4fcf2cfffe012d01fe
mjsqu <mj2.squ@gmail.com>
2022-09-16 13:35:27 +1200

    Fix missing quote
    typo

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='pipelinewise-tap-oracle',
           'Programming Language :: Python :: 3 :: Only'
       ],
       install_requires=[
-          'pipelinewise-singer-python @ git+https://github.com/mjsqu/pipelinewise-singer-python@feature/default_decimal_decimal,
+          'pipelinewise-singer-python @ git+https://github.com/mjsqu/pipelinewise-singer-python@feature/default_decimal_decimal',
           'cx_Oracle==8.3',
           'strict-rfc3339==0.7'
       ],
